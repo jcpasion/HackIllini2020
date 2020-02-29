51f669344681281a9148e4461f2608eccadce084
brentpm2 <brentpm2@illinois.edu>
2020-02-29 16:14:24 -0600

    fixed edge case where all values were the same causing an error

--- get_bin_sizes.py
+++ get_bin_sizes.py
@@ -1,18 +1,26 @@
+import numpy as np
+
 def get_bin_sizes(dset,num_bins,percent_threshold):
+    #uses bins to remove datapoints in a channel that are in underrepresented bins
+    #input: dset as an array of integers, number of bins, threshold
+    #output: array of integers where datapoints in underrepresented bins are replaced by 'None"
     bin_dist = np.histogram(dset,num_bins)
     bin_sizes = bin_dist[0]
-    bin_offset = (bin_dist[1][-1] - bin_dist[1][0]) / (num_bins - 1)
-    num_threshold  = len(dset) * percent_threshold
-    cull_between = {}
-    good_dset = []
-    #add bins to cull
-    for i in range(len(bin_sizes)):
-        if bin_sizes[i] < num_threshold:
-            cull_between[i] = 1
-    for entry in dset:
-        entry_bin = int((entry - bin_dist[1][0]) / bin_offset)
-        if entry_bin in cull_between:
-            good_dset.append(nan)
-        else:
-            good_dset.append(entry)
-    return good_dset
+    if (bin_dist[1][-1] - bin_dist[1][0]) > 0:
+        bin_offset = (bin_dist[1][-1] - bin_dist[1][0]) / (num_bins - 1)
+        num_threshold  = len(dset) * percent_threshold
+        cull_between = {}
+        good_dset = []
+        #add bins to cull
+        for i in range(len(bin_sizes)):
+            if bin_sizes[i] < num_threshold:
+                cull_between[i] = 1
+        for entry in dset:
+            entry_bin = int((entry - bin_dist[1][0]) / bin_offset)
+            if entry_bin in cull_between:
+                good_dset.append(np.nan)
+            else:
+                good_dset.append(entry)
+        return good_dset
+    else:
+        return dset
