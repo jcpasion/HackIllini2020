e6f18653d4fbcf9e9618ed854cd171737cca05a2
jcpasion <jcpasion@ucdavis.edu>
2020-02-29 17:07:07 -0600

    added historic filter

--- newer_hdf.py
+++ newer_hdf.py
@@ -97,6 +97,52 @@ def get_bin_sizes(dset,num_bins,percent_threshold):
     else:
         return dset
 
+def filter_channels_historic(in_dict,stdev_cutoff):
+    #get global distribution of summary stats for each channel
+    global_min = {}
+    global_max = {}
+    for key_file in in_dict:
+        for key_channel in in_dict[key_file]:
+            if key_channel in global_min:
+                global_min[key_channel].append(in_dict[key_file][key_channel]['min'])
+                global_max[key_channel].append(in_dict[key_file][key_channel]['max'])
+            else:
+                global_min[key_channel] = in_dict[key_file][key_channel]['min']
+                global_max[key_channel] = in_dict[key_file][key_channel]['max']
+    #process global_min
+    global_min_upper = {}
+    global_min_lower = {}
+    global_max_upper = {}
+    global_max_lower = {}
+    for key in global_min:
+        
+        global_min_mean = np.average(np.array(list(global_min.items()),dtype=np.float))
+        global_min_stdev = np.std(np.array(list(global_min.items()),dtype=np.float))
+        global_min_upper[key] = global_min_mean + (global_min_stdev * stdev_cutoff)
+        global_min_lower[key] = global_min_mean - (global_min_stdev * stdev_cutoff)
+    
+        #process global_max
+        global_max_mean = np.average(np.array(list(global_max.items()),dtype=np.float))
+        global_max_stdev = np.std(np.array(list(global_max.items()),dytype=np.float))
+        global_max_upper[key] = global_max_mean + (global_max_stdev * stdev_cutoff)
+        global_max_lower[key] = global_max_mean - (global_max_stdev * stdev_cutoff)
+    
+    for key_file in in_dict:
+        for key_channel in in_dict[key_file]:
+            if in_dict[key_file][key_channel]['min'] > global_min_upper[key_channel]:
+                del in_dict[key_file][key_channel]
+            elif in_dict[key_file][key_channel]['min'] < global_min_lower[key_channel]:
+                del in_dict[key_file][key_channel]
+            if in_dict[key_file][key_channel]['max'] > global_max_upper[key_channel]:
+                del in_dict[key_file][key_channel]
+            elif in_dict[key_file][key_channel]['max'] < global_max_lower[key_channel]:
+                del in_dict[key_file][key_channel]
+    return in_dict
+
+
+
+
+
 
 #initialize a dictionary of summary stats
 summary_stats = {}
@@ -118,7 +164,6 @@ for file in directory:
             
             #create Dictionary Keys for average, min, max
             summary_stats[file][dataset]={}
-            summary_stats[file][dataset]['average'] = {}
             summary_stats[file][dataset]['min'] = {}
             summary_stats[file][dataset]['max'] = {}
 
@@ -133,6 +178,10 @@ for file in directory:
         f.close()
 
 print (summary_stats)
+
+
+print(filter_channels_historic(summary_stats,2))
+
 
 output = open('summary.pkl', 'wb')
 pickle.dump(summary_stats, output)
@@ -234,49 +283,3 @@ print (list(f_attr.keys()))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-#get metadata for chanIDs
-chan_attr = chanIDs.attrs
-print (list(chan_attr.keys()))
-
-#get data and then metadata for single channel ch_86
-ch_86 = chanIDs['ch_86']
-print (list(ch_86.keys()))
-
-ch_86_attr = ch_86.attrs
-print (list(ch_86_attr.keys()))
-
-
-#the data within ch_86
-dset = ch_86['MEASURED']
-print (list((dset.attrs).keys()))
-
-#print sample rate
-print (ch_86_attr['SAMPLE RATE'])
-
-#metadata for whole file
-f_attr= f.attrs
-print (list(f_attr.keys()))
-'''
