9ae9e6c933c0f434f96fa0cfb06291c7b902c395
brentpm2 <brentpm2@illinois.edu>
2020-02-29 17:35:56 -0600

    Fixed error where dictionaries were not properly converted into arrays

--- filter_channels_historic.py
+++ filter_channels_historic.py
@@ -6,12 +6,13 @@ def filter_channels_historic(in_dict,stdev_cutoff):
     global_max = {}
     for key_file in in_dict:
         for key_channel in in_dict[key_file]:
-            if key_channel in global_min:
-                global_min[key_channel].append(in_dict[key_file][key_channel]['min'])
-                global_max[key_channel].append(in_dict[key_file][key_channel]['max'])
-            else:
-                global_min[key_channel] = in_dict[key_file][key_channel]['min']
-                global_max[key_channel] = in_dict[key_file][key_channel]['max']
+            if key_channel not in global_min:
+                global_min[key_channel] = []
+                global_max[key_channel] = []
+
+            global_min[key_channel].append(in_dict[key_file][key_channel]['min'])
+            global_max[key_channel].append(in_dict[key_file][key_channel]['max'])
+
     #process global_min
     global_min_upper = {}
     global_min_lower = {}
@@ -19,19 +20,21 @@ def filter_channels_historic(in_dict,stdev_cutoff):
     global_max_lower = {}
     for key in global_min:
         
-        global_min_mean = np.average(global_min)
-        global_min_stdev = np.std(global_min)
+        global_min_mean = np.average(np.array(list(global_min.values()),dtype=np.float))
+        global_min_stdev = np.std(np.array(list(global_min.values()),dtype=np.float))
         global_min_upper[key] = global_min_mean + (global_min_stdev * stdev_cutoff)
         global_min_lower[key] = global_min_mean - (global_min_stdev * stdev_cutoff)
     
         #process global_max
-        global_max_mean = np.average(global_max)
-        global_max_stdev = np.std(global_max)
+        global_max_mean = np.average(np.array(list(global_max.values()),dtype=np.float))
+        global_max_stdev = np.std(np.array(list(global_max.values()),dtype=np.float))
         global_max_upper[key] = global_max_mean + (global_max_stdev * stdev_cutoff)
         global_max_lower[key] = global_max_mean - (global_max_stdev * stdev_cutoff)
     
     for key_file in in_dict:
         for key_channel in in_dict[key_file]:
+            print(in_dict[key_file][key_channel]['min'])
+            print(global_min_upper[key_channel])
             if in_dict[key_file][key_channel]['min'] > global_min_upper[key_channel]:
                 del in_dict[key_file][key_channel]
             elif in_dict[key_file][key_channel]['min'] < global_min_lower[key_channel]:
