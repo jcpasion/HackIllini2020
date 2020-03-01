7ae32070d29a2fdfe66ce3fed10311f1f682940b
brentpm2 <brentpm2@illinois.edu>
2020-02-29 19:13:20 -0600

    Added comments

--- filter_channels_historic.py
+++ filter_channels_historic.py
@@ -2,10 +2,15 @@ import numpy as np
 
 def filter_channels_historic(in_dict,stdev_cutoff):
     #get global distribution of summary stats for each channel
+    #INPUT 1: in_dict, a dictionary of files and their channels
+    #INPUT 2: stdev_cutoff, a multiplier for the global stdev 
+    #         to remove file:channels with high deviance from mean
+    #OUTPUT 1: in_dict after filtering is applied
     global_min = {}
     global_max = {}
     for key_file in in_dict:
         for key_channel in in_dict[key_file]:
+            #populate values for min and max
             if key_channel not in global_min:
                 global_min[key_channel] = []
                 global_max[key_channel] = []
@@ -19,7 +24,7 @@ def filter_channels_historic(in_dict,stdev_cutoff):
     global_max_upper = {}
     global_max_lower = {}
     for key in global_min:
-        
+        #compute the global confidence interval
         global_min_mean = np.average(np.array(list(global_min.values()),dtype=np.float))
         global_min_stdev = np.std(np.array(list(global_min.values()),dtype=np.float))
         global_min_upper[key] = global_min_mean + (global_min_stdev * stdev_cutoff)
@@ -30,11 +35,9 @@ def filter_channels_historic(in_dict,stdev_cutoff):
         global_max_stdev = np.std(np.array(list(global_max.values()),dtype=np.float))
         global_max_upper[key] = global_max_mean + (global_max_stdev * stdev_cutoff)
         global_max_lower[key] = global_max_mean - (global_max_stdev * stdev_cutoff)
-    
+    #find file:channels which are more extreme than allowed interval
     for key_file in in_dict:
         for key_channel in in_dict[key_file]:
-            print(in_dict[key_file][key_channel]['min'])
-            print(global_min_upper[key_channel])
             if in_dict[key_file][key_channel]['min'] > global_min_upper[key_channel]:
                 del in_dict[key_file][key_channel]
             elif in_dict[key_file][key_channel]['min'] < global_min_lower[key_channel]:
